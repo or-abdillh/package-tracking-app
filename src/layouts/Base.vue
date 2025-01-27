41d1de81d2b67a09a574605d270e4104d0c506e5
or-abdillh <hans.abdillh05@gmail.com>
2025-01-27 17:04:16 +0800

    feat: create Base layout component with BottomBar integration

---
+++ src/layouts/Base.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="w-full bg-black min-h-screen">
+        <!-- fixed wrapper -->
+        <main class="md:w-5/12 lg:w-4/12 xl:w-3/12 md:mx-auto">
+            <slot />
+        </main>
+
+        <!-- bottom bar -->
+        <BottomBar />
+    </main>
+</template>
+
+<script setup lang="ts">
+
+import BottomBar from "@/components/BottomBar.vue"
+
+</script>
